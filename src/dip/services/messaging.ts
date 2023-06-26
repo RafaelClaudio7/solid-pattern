5ec34392f31478ab9d4c405fc81888e7ad5e5929
RafaelClaudio7 <rc7true@gmail.com>
2023-06-26 18:50:48 -0300

    Dependency inversion principle

---
+++ src/dip/services/messaging.ts
@@ -0,0 +1,7 @@
+import { MessagingProtocol } from '../entities/interfaces/messaging-protocol';
+
+export class Messaging implements MessagingProtocol {
+  sendMessage(message: string): void {
+    console.log('Mensagem: ', message);
+  }
+}
