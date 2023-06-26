782237ef0420823405d706eaa56bd7ed914aae56
RafaelClaudio7 <rc7true@gmail.com>
2023-06-26 17:52:55 -0300

    Interface segregation principle

--- src/isp/main.ts
+++ src/isp/main.ts
@@ -1,6 +1,9 @@
 /*
-Interface segregation principle (Princípio da segregação de Interface) -
-os clientes não devem ser forçados a depender de interfaces que não utilizam
+Liskov substituition principle (Princípio da substituição de Liskov)
+Se ɸ(x) é uma propriedade  demonstrável dos objetos x de tipo T. Então ɸ(x)
+deve ser verdadeiro para objetos y de tipo S onde S é um subtipo de T
+
+Simplificando: Subtipos precisam ser substituíveis por seus tipos de base
 */
 
 import { Messaging } from './services/messaging';
@@ -9,6 +12,7 @@ import { Persistency } from './services/persistency';
 import { Product } from './entities/product';
 import { ShoppingCart } from './useCases/shopping-cart';
 import { NoDiscount } from './entities/discount';
+import { EnterpriseCustomer, IndividualCustomer } from './entities/customer';
 
 // const fiftyPercentDiscount = new FiftyPercentDiscount();
 // const tenPercentDiscount = new TenPercentDiscount();
@@ -17,7 +21,19 @@ const shoppingCart = new ShoppingCart(noDiscount);
 
 const messaging = new Messaging();
 const persistency = new Persistency();
-const order = new Order(shoppingCart, messaging, persistency);
+
+const customerPerson = new IndividualCustomer(
+  'Rafael',
+  'Tula',
+  '146.101.776-92',
+);
+
+const customerEnterprise = new EnterpriseCustomer(
+  'Microsoft',
+  '111.111.111.111-11',
+);
+
+const order = new Order(shoppingCart, messaging, persistency, customerPerson);
 
 shoppingCart.addItem(new Product('Arroz', 21.95));
 shoppingCart.addItem(new Product('Camisa', 99.9));
